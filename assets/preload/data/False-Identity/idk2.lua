c49020a51f22ec5fb3afd4993914ff1f5537bc37
HeitorzinKkkj <117235821+HeitorzinKkkj@users.noreply.github.com>
2024-02-08 22:35:48 -0300

    Delete assets/preload/data/False-Identity/idk2.lua

--- assets/preload/data/False-Identity/idk2.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-makeAnimatedLuaSprite('optwavesA', 'optwavesA', 0, 0);
-luaSpriteAddAnimationByPrefix('optwavesA', 'idle', 'idle', 24, true);
-luaSpritePlayAnimation('optwavesA', 'idle');
-addLuaSprite('optwavesA', false);
-scaleObject('optwavesA', 2.5, 2.5);
-setObjectCamera('optwavesA', 'other')
-end
